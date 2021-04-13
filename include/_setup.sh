08e8a42af1392dda1bf82d5e4b87eac2c5ca7e7b
Andreas Kirsch <blackhc@gmail.com>
2021-04-13 09:39:14 +0100

    Add support for a local _setup.sh

--- include/_setup.sh
+++ include/_setup.sh
@@ -11,6 +11,7 @@ set -e
 # Uncomment to enable tracing
 set -x
 
+# shellcheck source=./arc/_setup_config.sh
 . ${SLURM_SCRIPT_INCLUDE}/${CLOUD_TYPE}/_setup_config.sh
 
 export BUILD_DIR=$TMPDIR/pip-build
@@ -18,13 +19,16 @@ export BUILD_DIR=$TMPDIR/pip-build
 conda config --add channels conda-forge
 conda config --set channel_priority flexible
 
-conda create -q -y -p "$CURRENT_CONDA_ENV_PATH" python=${PYTHON_VERSION}
-source activate "$CURRENT_CONDA_ENV_PATH"
+conda create -q -y -p "{$CURRENT_CONDA_ENV_PATH}" "python=${PYTHON_VERSION}"
+source activate "{$CURRENT_CONDA_ENV_PATH}"
 
 : ${CONDA_ENV_FILE:="slurm-conda.yml"}
+: ${MANUAL_SETUP:="_setup.sh"}
 
-if test -f "$CONDA_ENV_FILE"; then
-  conda env update -y --file ${CONDA_ENV_FILE}
+if test -f "${MANUAL_SETUP}"; then
+  "{$MANUAL_SETUP}"
+elif test -f "${CONDA_ENV_FILE}"; then
+  conda env update -y --file "${CONDA_ENV_FILE}"
 else
   conda install -y pytorch=1.8.1 torchvision cudatoolkit=10.2 ignite -c pytorch
 fi
